8c5ac79767a7cea99b539c021a0f9e6b358db860
Marco "Efesto" Polita <polita.marco@gmail.com>
2019-12-01 19:26:45 +0100

    adds  a server for serving readings

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 install-service:
 	sudo cp humidity.service /etc/systemd/system
 	sudo systemctl enable humidity.service
+
+install-deps:
+	pip install Adafruit_Python_DHT
+
 run:
 	python3 humidity.py
--- humidity.py
+++ humidity.py
@@ -2,26 +2,49 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
+
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 17
 INTERVAL = 60 * 10
 
-FILE_PATH="~/share/readings.csv"
+SERVER_PORT = 8080
+
+FILE_PATH="/home/pi/share/readings.csv"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+class FileHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+
+			with open(FILE_PATH, 'rb') as file:
+				content = file.read()	.decode("utf-8").replace("\n", "<br />")			
+        self.wfile.write(content)
+
+with socketserver.TCPServer(("", SERVER_PORT), FileHandler) as httpd:
+    print("serving at port", SERVER_PORT)
+    httpd.serve_forever()
+
 try:
 	while True:
-		file = open(FILE_PATH, "a")
-		humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+		with open(FILE_PATH, 'a') as file: 
+			humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-		if humidity is not None and temperature is not None:
-			line = "{0} {1} {2:0.1f}*C {3:0.1f}%\n".format(time.strftime('%d/%m/%y'), time.strftime('%H:%M'), temperature, humidity)
-			print(line)
-			file.write(line)
-		else:
-			print("Nope")
-		file.close()
+			if humidity is not None and temperature is not None:
+				now_time = time.strftime('%d/%m/%y')
+				today = time.strftime('%H:%M')
+
+				line = "{0} {1} {2:0.1f}*C {3:0.1f}%\n".format(now_time, today, temperature, humidity)
+
+				print(line)
+				file.write(line)
+			else:
+				print("Nope")
 		time.sleep(INTERVAL)
 except KeyboardInterrupt:	
 	pass
