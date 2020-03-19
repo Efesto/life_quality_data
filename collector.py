7b1b02a04859406cbe6674a149df068682e0660a
Marco "Efesto" Polita <polita.marco@gmail.com>
2020-03-19 20:21:17 +0100

    collector uses sensor class

--- collector.py
+++ collector.py
@@ -1,21 +1,12 @@
-import Adafruit_DHT
-import RPi.GPIO as GPIO
 import time
 from repository import mysql
-
-
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 17
-INTERVAL = 60 * 10
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+from sensors import dht
 
 try:
 	while True:
 		repo = mysql.MySql()
 	
-		humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)		
+		humidity, temperature = dht.DHT().read()
 
 		if humidity is not None and temperature is not None:
 			repo.insert_reading(time, temperature, humidity)			
