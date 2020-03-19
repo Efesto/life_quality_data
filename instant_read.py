9cc8809463cc0a36c7a8c2fb038315338df77e01
Marco "Efesto" Polita <polita.marco@gmail.com>
2020-03-19 20:15:29 +0100

    extracts humidity sensor in class

--- instant_read.py
+++ instant_read.py
@@ -1,14 +1,6 @@
-import Adafruit_DHT
-import RPi.GPIO as GPIO
-
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 17
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+from repository import dht
 
 try:
-	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-	print("{}C {}%".format(temperature, humidity))
+	print("{}C {}%".format(dht.DHT().read()))
 except KeyboardInterrupt:
 	pass
---
+++ sensors/dht.py
@@ -0,0 +1,12 @@
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+class DHT:
+	def read(self):
+        return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
